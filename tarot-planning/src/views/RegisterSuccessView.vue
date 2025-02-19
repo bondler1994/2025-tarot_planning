7a69bf80a1c2f45c043489d589a8e963f4262d73
CC Tung <maple51327@gmail.com>
2025-02-19 17:48:21 +0800

    feat: success views

---
+++ tarot-planning/src/views/RegisterSuccessView.vue
@@ -0,0 +1,39 @@
+<script setup></script>
+
+<template>
+  <div class="register column content-center">
+    <div class="register__title">
+      <div class="title">驗證成功</div>
+    </div>
+    <div class="register__description">
+      <div class="description">請稍等，正在製作你的專屬日記...</div>
+      <div class="description">製作完成後會直接跳至會員日記頁面！</div>
+    </div>
+    <div class="register__image">
+      <div class="image">
+        <img src="https://fakeimg.pl/274x100/" alt="register success" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/sass/font.scss';
+.register {
+  background-color: $grey-4;
+
+  padding: 24px;
+
+  width: 322px;
+
+  gap: 24px;
+}
+
+.title {
+  font-size: font.$custom-h2;
+}
+
+.description {
+  font-size: font.$custom-h6;
+}
+</style>
