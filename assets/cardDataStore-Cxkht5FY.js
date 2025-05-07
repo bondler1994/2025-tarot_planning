606f4d5e46481fd07c9845613866d0ba62257705
Watson <bbcallyou2@gmail.com>
2025-05-07 16:49:56 +0800

    Updates

---
+++ assets/cardDataStore-Cxkht5FY.js
@@ -0,0 +1,1 @@
+import{al as s,r as t}from"./index-DpX1dd_-.js";const o=s("card",()=>{const a=t({id:null,name:"",is_upright:null,blessing_message:""});function e(r){a.value={...r}}return{cardData:a,setCardData:e}});export{o as u};
