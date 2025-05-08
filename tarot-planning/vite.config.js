55354ccc29059bf19fc9919660a65f8eb8c8cba5
Watson <bbcallyou2@gmail.com>
2025-05-08 13:33:24 +0800

    base delete

--- tarot-planning/vite.config.js
+++ tarot-planning/vite.config.js
@@ -7,7 +7,7 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/2025-tarot_planning/',
+  base: '',
   plugins: [
     vue({
       template: { transformAssetUrls },
