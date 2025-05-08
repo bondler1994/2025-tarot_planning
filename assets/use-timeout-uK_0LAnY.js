efacb95442dc5d25b70427c6b1d47bd8f1e44e2b
Watson <bbcallyou2@gmail.com>
2025-05-08 11:35:19 +0800

    Updates

--- assets/use-timeout-uK_0LAnY.js
+++
@@ -1,1 +0,0 @@
-import{e as u}from"./QBtn-Dl7c_3cQ.js";import{aA as m,m as i,g as s}from"./index-DpX1dd_-.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return m(t),i(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
