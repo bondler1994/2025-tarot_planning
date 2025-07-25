5037fda61b2168688d10e093beb8daf1c5e56987
Jeremy Kuo <860824jeremy@gmail.com>
2025-07-25 15:12:08 +0800

    feat(auth): auto-refresh JWT & auto-logout when expired

--- tarot-planning/src/features/tarotDiaryAPI.js
+++ tarot-planning/src/features/tarotDiaryAPI.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import { decodeExp } from './decodeJWT'
+import dayjs from 'dayjs'
+import router from '@/router'
 
 const API_URL = 'https://api-tarotdiary.codeteches.com/'
 
@@ -9,6 +12,56 @@ const apiClient = axios.create({
     'Content-Type': 'application/json',
   },
 })
+
+apiClient.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem('accessToken')
+
+    if (!token) return config
+
+    const expirationTime = decodeExp(token)
+    const remainingSecond = expirationTime.diff(dayjs(), 's')
+
+    if (remainingSecond >= 60) {
+      config.headers.Authorization = `Bearer ${token}`
+    } else if (remainingSecond < 60) {
+      const newToken = await refreshAccess(token)
+      config.headers.Authorization = `Bearer ${newToken}`
+    } else {
+      throw new Error('Unknown error!')
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+const refreshClient = axios.create({
+  baseURL: API_URL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+})
+
+async function refreshAccess(oldToken) {
+  try {
+    const { data } = await refreshClient.post(
+      '/api/auth/refresh',
+      {},
+      {
+        headers: { Authorization: `Bearer ${oldToken}` },
+      },
+    )
+    localStorage.setItem('accessToken', data.token)
+    return data.token
+  } catch (e) {
+    localStorage.removeItem('accessToken')
+    router.replace({ name: 'login' })
+    throw e
+  }
+}
 
 export default {
   async GET(url) {
